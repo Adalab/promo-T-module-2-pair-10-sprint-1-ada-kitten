1405aa5e311547a7f128ba578ea588c08d8219c3
raquelgarciat <raquelgarciat@gmail.com>
2023-05-22 11:26:17 +0200

    add conditionals

--- main.js
+++ main.js
@@ -56,3 +56,22 @@ const kittenThree = `<li class="card">
 const catList = document.querySelector('.js-list');
 catList.innerHTML = kittenOne+kittenTwo+kittenThree;
 
+
+const input_search_desc = document.querySelector('.js_in_search_desc');
+const descrSearchText = input_search_desc.value;
+
+const kittenDesc1 = 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+const kittenDesc2 = 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
+const kittenDesc3 = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
+
+if( kittenDesc1.includes(descrSearchText) ) {
+  catList.innerHTML = innerHTML+kittenOne;
+  }
+  
+  else if( kittenDesc2.includes(descrSearchText) ) {
+    catList.innerHTML = innerHTML+kittenTwo;
+  }
+  
+  else if( kittenDesc3.includes(descrSearchText) ) {
+    catList.innerHTML = innerHTML+kittenThree;
+  }
